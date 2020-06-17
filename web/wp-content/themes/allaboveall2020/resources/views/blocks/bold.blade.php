ef705d5e7a43eeba76e4fa186c68e19992619961
Casey Fox <cfox612@gmail.com>
2020-06-17 13:07:06 -0400

    bold block tweaks

--- web/wp-content/themes/allaboveall2020/resources/views/blocks/bold.blade.php
+++ web/wp-content/themes/allaboveall2020/resources/views/blocks/bold.blade.php
@@ -11,61 +11,61 @@
   SupportsMode: false
   SupportsMultiple: true
 --}}
-<div class="bold-block">
+<div class="bold-block bold-tall">
   <div class="container">
     <div class="row">
-      <div class="col-lg-6 offset-lg-3 top-text text-center">
-        <h2 class="text-white text-uppercase">{!! $block['data']['bold_title'] !!}</h2>
-        <p class="text-white text-large">{!! $block['data']['bold_text'] !!}</p>
+      <div class="col-lg-8 offset-lg-2 top-text text-center">
+        <h2 class="text-uppercase">{!! $block['data']['bold_title'] !!}</h2>
+        <p class="text-large">{!! $block['data']['bold_text'] !!}</p>
       </div>
     </div>
       <div class="row">
-        <div class="col-lg-6 offset-lg-3">
+        <div class="col-lg-8 offset-lg-2">
           <div class="letter-block">
             <div class="letter">B</div>
             <div class="content">
               <h3 class="text-uppercase">{!! $block['data']['b_info_b_title'] !!}</h3>
               <p>{!! $block['data']['b_info_b_text'] !!}</p>
               @if(!empty($block['data']['b_info_b_link']['url']))
-                <a href="{!! $block['data']['b_info_b_link']['url'] !!}">Learn More</a>
+                <a href="{!! $block['data']['b_info_b_link']['url'] !!}" class="btn btn-white-outline">Learn More</a>
               @endif
             </div>
           </div>
         </div>
-        <div class="col-lg-6 offset-lg-3">
+        <div class="col-lg-8 offset-lg-2">
           <div class="letter-block">
             <div class="letter">O</div>
             <div class="content">
               <h3 class="text-uppercase">{!! $block['data']['o_info_o_title'] !!}</h3>
               <p>{!! $block['data']['o_info_o_text'] !!}</p>
               @if(!empty($block['data']['o_info_o_link']['url']))
-                <a href="{!! $block['data']['o_info_o_link']['url'] !!}">Learn More</a>
+                <a href="{!! $block['data']['o_info_o_link']['url'] !!}" class="btn btn-white-outline">Learn More</a>
               @endif
             </div>
           </div>
         </div>
       </div>
       <div class="row">
-        <div class="col-lg-6 offset-lg-3">
+        <div class="col-lg-8 offset-lg-2">
           <div class="letter-block">
             <div class="letter">L</div>
             <div class="content">
               <h3 class="text-uppercase">{!! $block['data']['l_info_l_title'] !!}</h3>
               <p>{!! $block['data']['l_info_l_text'] !!}</p>
               @if(!empty($block['data']['l_info_l_link']['url']))
-                <a href="{!! $block['data']['l_info_l_link']['url'] !!}">Learn More</a>
+                <a href="{!! $block['data']['l_info_l_link']['url'] !!}" class="btn btn-white-outline">Learn More</a>
               @endif
             </div>
           </div>
         </div>
-        <div class="col-lg-6 offset-lg-3">
+        <div class="col-lg-8 offset-lg-2">
           <div class="letter-block">
             <div class="letter">D</div>
             <div class="content">
               <h3 class="text-uppercase">{!! $block['data']['d_info_d_title'] !!}</h3>
               <p>{!! $block['data']['d_info_d_text'] !!}</p>
               @if(!empty($block['data']['d_info_d_link']['url']))
-                <a href="{!! $block['data']['d_info_d_link']['url'] !!}">Learn More</a>
+                <a href="{!! $block['data']['d_info_d_link']['url'] !!}" class="btn btn-white-outline">Learn More</a>
               @endif
             </div>
           </div>
