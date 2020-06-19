6503f4e26b8d6baa01e94974a2e6c175a28991b9
Casey Fox <cfox612@gmail.com>
2020-06-19 08:25:55 -0400

    tweaking container

--- web/wp-content/themes/allaboveall2020/resources/views/page--container.blade.php
+++ web/wp-content/themes/allaboveall2020/resources/views/page--container.blade.php
@@ -10,7 +10,7 @@
     @section('content')
       @while(have_posts()) @php the_post() @endphp
         @include('partials.page-header')
-        @include('partials.content-page-container')
+        @include('partials.content-page')
       @endwhile
     @endsection
   </div>
