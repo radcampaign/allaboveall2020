9b4f333ed62f9c70e453d42ec2cc3495b2880958
Casey Fox <cfox612@gmail.com>
2020-07-13 12:40:00 -0400

    #72 2 col bold block link letters

--- web/wp-content/themes/allaboveall2020/resources/views/blocks/bold-full.blade.php
+++ web/wp-content/themes/allaboveall2020/resources/views/blocks/bold-full.blade.php
@@ -32,7 +32,7 @@
         </div>
         <div class="col-lg-6">
           <div class="letter-block">
-            <div class="letter"><a class="letter-link" href="{!! $block['data']['b_info_b_link']['url'] !!}">O</a></div>
+            <div class="letter"><a class="letter-link" href="{!! $block['data']['o_info_o_link']['url'] !!}">O</a></div>
             <div class="content">
               <h3 class="text-uppercase mt-0">{!! $block['data']['o_info_o_title'] !!}</h3>
               <p>{!! $block['data']['o_info_o_text'] !!}</p>
@@ -44,7 +44,7 @@
       <div class="row">
         <div class="col-lg-6">
           <div class="letter-block">
-            <div class="letter"><a class="letter-link" href="{!! $block['data']['b_info_b_link']['url'] !!}">L</a></div>
+            <div class="letter"><a class="letter-link" href="{!! $block['data']['l_info_l_link']['url'] !!}">L</a></div>
             <div class="content">
               <h3 class="text-uppercase mt-0">{!! $block['data']['l_info_l_title'] !!}</h3>
               <p>{!! $block['data']['l_info_l_text'] !!}</p>
@@ -54,7 +54,7 @@
         </div>
         <div class="col-lg-6">
           <div class="letter-block">
-            <div class="letter"><a class="letter-link" href="{!! $block['data']['b_info_b_link']['url'] !!}">D</a></div>
+            <div class="letter"><a class="letter-link" href="{!! $block['data']['d_info_d_link']['url'] !!}">D</a></div>
             <div class="content">
               <h3 class="text-uppercase mt-0">{!! $block['data']['d_info_d_title'] !!}</h3>
               <p>{!! $block['data']['d_info_d_text'] !!}</p>
