242cb6bd346be42bcf7d27a12a967877bfead55e
Casey Fox <cfox612@gmail.com>
2020-07-17 15:12:37 -0400

    join tweaks

--- web/wp-content/themes/allaboveall2020/resources/views/blocks/join-two-col.blade.php
+++ web/wp-content/themes/allaboveall2020/resources/views/blocks/join-two-col.blade.php
@@ -11,14 +11,26 @@
   SupportsMode: false
   SupportsMultiple: true
 --}}
-
-<div class="container">
-  <div class="row join-embed">
-    <div class="col-lg-3 offset-lg-1">
-      <h2>{!! $block['data']['left_headline'] !!}</h2>
-    </div>
-    <div class="col-lg-5 offset-lg-1">
-      {!! $block['data']['right_embed'] !!}
+@php
+  $asterisk = 'no-asterisk';
+  if($block['data']['asterisk'][0] == '1') {
+    $asterisk = 'asterisk-yes asterisk-right join-asterisk';
+    $asteriskblock = '<div class="asterisk-container"><img src="/wp-content/uploads/2020/04/asterisk-green.png"></div>';
+  }
+  
+@endphp
+<div class="data-{{ $block['id'] }} {{ $asterisk }}">
+  <div class="full-width-text-inner">
+    {!! $asteriskblock !!}
+    <div class="container container-inner">
+      <div class="row join-embed">
+        <div class="col-lg-3 offset-lg-1">
+          <h2>{!! $block['data']['left_headline'] !!}</h2>
+        </div>
+        <div class="col-lg-5 offset-lg-1">
+          {!! $block['data']['right_embed'] !!}
+        </div>
+      </div>
     </div>
   </div>
 </div>
