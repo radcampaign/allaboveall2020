f7dc95c7943ff590b6dc47f5b9408f0809f25afb
Casey Fox <cfox612@gmail.com>
2020-08-07 15:28:45 -0400

    blade fix

--- web/wp-content/themes/allaboveall2020/resources/views/blocks/bold-full.blade.php
+++ web/wp-content/themes/allaboveall2020/resources/views/blocks/bold-full.blade.php
@@ -22,21 +22,33 @@
       <div class="row">
         <div class="col-lg-6">
           <div class="letter-block">
-            <div class="letter"><a class="letter-link" href="{!! $block['data']['b_info_b_link']['url'] !!}">B<a></div>
+            @if(!empty($block['data']['b_info_b_link']['url']))
+              <div class="letter"><a class="letter-link" href="{!! $block['data']['b_info_b_link']['url'] !!}">B</a></div>
+            @else
+              <div class="letter">B</div>
+            @endif
             <div class="content">
               <h3 class="text-uppercase mt-0">{!! $block['data']['b_info_b_title'] !!}</h3>
               <p>{!! $block['data']['b_info_b_text'] !!}</p>
-              <a href="{!! $block['data']['b_info_b_link']['url'] !!}">Learn More</a>
+              @if(!empty($block['data']['b_info_b_link']['url']))
+                <a href="{!! $block['data']['b_info_b_link']['url'] !!}">Learn More</a>
+              @endif
             </div>
           </div>
         </div>
         <div class="col-lg-6">
           <div class="letter-block">
-            <div class="letter"><a class="letter-link" href="{!! $block['data']['o_info_o_link']['url'] !!}">O</a></div>
+            @if(!empty($block['data']['o_info_o_link']['url']))
+              <div class="letter"><a class="letter-link" href="{!! $block['data']['o_info_o_link']['url'] !!}">O</a></div>
+            @else
+              <div class="letter">O</div>
+            @endif
             <div class="content">
               <h3 class="text-uppercase mt-0">{!! $block['data']['o_info_o_title'] !!}</h3>
               <p>{!! $block['data']['o_info_o_text'] !!}</p>
-              <a href="{!! $block['data']['o_info_o_link']['url'] !!}">Learn More</a>
+              @if(!empty($block['data']['o_info_o_link']['url']))
+                <a href="{!! $block['data']['o_info_o_link']['url'] !!}">Learn More</a>
+              @endif
             </div>
           </div>
         </div>
@@ -44,21 +56,33 @@
       <div class="row">
         <div class="col-lg-6">
           <div class="letter-block">
-            <div class="letter"><a class="letter-link" href="{!! $block['data']['l_info_l_link']['url'] !!}">L</a></div>
+            @if(!empty($block['data']['l_info_l_link']['url']))
+              <div class="letter"><a class="letter-link" href="{!! $block['data']['l_info_l_link']['url'] !!}">L</a></div>
+            @else
+              <div class="letter">L</div>
+            @endif
             <div class="content">
               <h3 class="text-uppercase mt-0">{!! $block['data']['l_info_l_title'] !!}</h3>
               <p>{!! $block['data']['l_info_l_text'] !!}</p>
-              <a href="{!! $block['data']['l_info_l_link']['url'] !!}">Learn More</a>
+              @if(!empty($block['data']['l_info_l_link']['url']))
+                <a href="{!! $block['data']['l_info_l_link']['url'] !!}">Learn More</a>
+              @endif
             </div>
           </div>
         </div>
         <div class="col-lg-6">
           <div class="letter-block">
-            <div class="letter"><a class="letter-link" href="{!! $block['data']['d_info_d_link']['url'] !!}">D</a></div>
+            @if(!empty($block['data']['d_info_d_link']['url']))
+              <div class="letter"><a class="letter-link" href="{!! $block['data']['d_info_d_link']['url'] !!}">D</a></div>
+            @else
+              <div class="letter">D</div>
+            @endif
             <div class="content">
               <h3 class="text-uppercase mt-0">{!! $block['data']['d_info_d_title'] !!}</h3>
               <p>{!! $block['data']['d_info_d_text'] !!}</p>
-              <a href="{!! $block['data']['d_info_d_link']['url'] !!}">Learn More</a>
+              @if(!empty($block['data']['d_info_d_link']['url']))
+                <a href="{!! $block['data']['d_info_d_link']['url'] !!}">Learn More</a>
+              @endif
             </div>
           </div>
         </div> 
