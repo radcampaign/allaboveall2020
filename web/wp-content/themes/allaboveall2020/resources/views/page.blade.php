f188d5dbba5e4ab377e067d6b5d3e26767e1eb39
Casey Fox <cfox612@gmail.com>
2020-08-21 10:25:07 -0400

    template

--- web/wp-content/themes/allaboveall2020/resources/views/page.blade.php
+++ web/wp-content/themes/allaboveall2020/resources/views/page.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.app')
-<div class="content">
       @section('content')
         @while(have_posts()) @php the_post() @endphp
           @include('partials.page-header')
           @include('partials.content-page')
         @endwhile
       @endsection
-</div>
