79aaaaa90e8abf60a70e79687dddf809de9316be
Casey Fox <cfox612@gmail.com>
2020-08-21 08:31:06 -0400

    theming

--- web/wp-content/themes/allaboveall2020/resources/views/partials/head.blade.php
+++ web/wp-content/themes/allaboveall2020/resources/views/partials/head.blade.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta property="fb:app_id" content="379766426342880"/>
   @php wp_head() @endphp
 </head>
