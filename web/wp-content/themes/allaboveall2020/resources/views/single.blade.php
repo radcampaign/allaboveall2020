5479aa4400b82ac659006082466b82787624ee8c
Casey Fox <cfox612@gmail.com>
2020-08-21 08:36:02 -0400

    theming

--- web/wp-content/themes/allaboveall2020/resources/views/single.blade.php
+++ web/wp-content/themes/allaboveall2020/resources/views/single.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.app')
-<div class="content">
 @section('content')
   @while(have_posts()) @php the_post() @endphp
     @include('partials.content-single-'.get_post_type())
   @endwhile
 @endsection
-</div>
